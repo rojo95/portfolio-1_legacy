e8689f5190480db08084eb924ea8ecfd769ab5ab
Johan Román <r.johan95@gmail.com>
2022-11-17 16:53:02 -0400

    correccion de rutas de navegacion, configurar vite para ejecucion en git pages y proyecto compilado para su despliegue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/AboutMe/index.vue'
 import Experience from '../views/experience/index.vue';
-import About from '../views/AboutView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
