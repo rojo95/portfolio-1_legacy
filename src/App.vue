769f6cdf5d9db5891678ef336c85dae411ad4aec
Johan Román <r.johan95@gmail.com>
2025-04-29 18:52:08 -0400

    style: agregar datos al timeline de trabajos, borrar boton de descarga de curriculum

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
-  import Footer from './components/footer/index.vue'
-  import NavBar from './components/navBar/index.vue'
-  M.AutoInit();
+import { RouterLink, RouterView } from "vue-router";
+import Footer from "./components/footer/index.vue";
+import NavBar from "./components/navBar/index.vue";
+M.AutoInit();
 </script>
 <script>
 export default {
   data() {
     return {
-      show : false,
-    }
+      show: false,
+    };
   },
   mounted() {
     this.show = !this.show;
   },
   methods: {
-    download () {
-      const file = "Curriculum_Johan_Roman-ES.pdf"
-      const pdf = new URL(`./assets/pdf/${file}`, import.meta.url).href
+    download() {
+      const file = "Curriculum_Johan_Roman-ES.pdf";
+      const pdf = new URL(`./assets/pdf/${file}`, import.meta.url).href;
       fetch(`${pdf}`)
-      .then(response => response.blob())
-      .then(blob => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', file);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch(error => console.error(error));
-    }
+        .then((response) => response.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(new Blob([blob]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", file);
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        })
+        .catch((error) => console.error(error));
+    },
   },
-}
+};
 </script>
 
 <template>
@@ -43,7 +43,7 @@ export default {
       </div>
       <div class="row">
         <div class="col s12 body">
-          <RouterView v-show="show"/>
+          <RouterView v-show="show" />
           <div>
             <div class="row" v-show="!show">
               <div class="col valign-wrapper s12 content">
@@ -52,9 +52,11 @@ export default {
                     <div class="spinner-layer spinner-green-only">
                       <div class="circle-clipper left">
                         <div class="circle"></div>
-                      </div><div class="gap-patch">
+                      </div>
+                      <div class="gap-patch">
                         <div class="circle"></div>
-                      </div><div class="circle-clipper right">
+                      </div>
+                      <div class="circle-clipper right">
                         <div class="circle"></div>
                       </div>
                     </div>
@@ -69,17 +71,6 @@ export default {
         <div class="col s12 p-none">
           <Footer />
         </div>
-      </div>
-      
-      <div class="fixed-action-btn" @click="download()">
-        <a class="btn-floating btn-large purple" title="Más">
-          <i class="large material-icons">file_download</i>
-          <!--i class="large material-icons">add</i-->
-        </a>
-        <!--ul>
-          <li><a class="btn-floating green"><i class="material-icons">visibility</i></a></li>
-          <li><a class="btn-floating blue" @click="download()"><i class="material-icons">file_download</i></a></li>
-        </ul-->
       </div>
     </div>
   </div>
--- src/components/WorkTimeline/index.vue
+++ src/components/WorkTimeline/index.vue
@@ -1,90 +1,124 @@
-
 <script setup>
-  import ItemTimeline from './ItemTimeline.vue';
-  import axios from 'redaxios';
+import ItemTimeline from "./ItemTimeline.vue";
+import axios from "redaxios";
 </script>
 
 <script>
-  export default {
-    data() {
-      return {
-        works: null
-      }
+export default {
+  data() {
+    return {
+      works: null,
+    };
+  },
+  mounted() {
+    this.getWorks();
+  },
+  methods: {
+    async getWorks() {
+      return await axios
+        .get(
+          new URL("../../assets/information/works.json", import.meta.url).href
+        )
+        .then((result) => {
+          this.works = result.data.data;
+        });
     },
-    mounted() {
-        this.getWorks();
-    },
-    methods: {
-      async getWorks() {
-            return await axios.get(new URL("../../assets/information/works.json", import.meta.url).href)
-            .then((result) => {
-                this.works = result.data.data;
-            })
-        }
-    }
-  };
+  },
+};
 </script>
 
 <template>
   <div class="content">
-
     <ItemTimeline v-for="item in works" v-bind:key="item">
       <template #icon>
         <i class="material-icons icon">business</i>
       </template>
       <template #heading>
-        <strong>
-          {{item.cargo}}
-        </strong>
-        &nbsp;- {{item.abrName}} <br> ({{item.inicio}} - {{item.fin || "Actualidad"}})
+        <div class="text-title">
+          <strong>
+            {{ item.cargo }}
+          </strong>
+          &nbsp;- {{ item.abrName }} <br />
+          ({{ item.inicio }} - {{ item.fin || "Actualidad" }})
+        </div>
       </template>
-  
+
       <div>
-        <b v-if="item.abrName!=item.name">{{item.name}}</b>
-        <br v-if="item.abrName!=item.name">
-        <br v-if="item.abrName!=item.name">
+        <div v-if="item.abrName != item.name">
+          <b>
+            <h6>{{ item.name }}</h6>
+          </b>
+          <br />
+        </div>
+        <div v-if="item.proyects">
+          <b>Proyectos:</b>
+          <br />
+          <p>
+            <ul>
+              <li v-for="(proy, i) in item.proyects" :key="i">
+                <b>
+                  Proyecto: {{ proy.name }}
+                </b>
+                <p>Rol: {{ proy.rol }}</p>
+                <p>
+                  {{ proy.desc }}
+                </p>
+                <p>
+                  {{ proy.tech }}
+                </p>
+                <p>
+                  {{ proy.goals }}
+                </p>
+              </li>
+            </ul>
+          </p>
+        </div>
+        <br v-if="item.proyects" />
         <b>Tareas Desempeñadas:</b>
         <ul>
-          <li v-for="list in item.funciones" v-bind:key="list">- {{list}}</li>
+          <li v-for="list in item.funciones" v-bind:key="list">- {{ list }}</li>
         </ul>
-  
-        <br>
+
+        <br />
         <div class="row">
-          <div class="col m6" v-if="item.conocimientos.length>0">
+          <div class="col m6" v-if="item.conocimientos?.length > 0">
             <b>Conocimientos Empleados:</b>
             <ul>
-              <li v-for="list in item.conocimientos" v-bind:key="list">- {{list}}</li>
+              <li v-for="list in item.conocimientos" v-bind:key="list">
+                - {{ list }}
+              </li>
             </ul>
           </div>
-          <div class="col m6" v-if="item.frameworks.length>0">
+          <div class="col m6" v-if="item.frameworks?.length > 0">
             <b>Frameworks Empleados:</b>
             <ul>
-              <li v-for="list in item.frameworks" v-bind:key="list">- {{list}}</li>
+              <li v-for="list in item.frameworks" v-bind:key="list">
+                - {{ list }}
+              </li>
             </ul>
           </div>
-          <div class="col m6" v-if="item.cms?.length>0">
+          <div class="col m6" v-if="item.cms?.length > 0">
             <b>CMS Empleados:</b>
             <ul>
-              <li v-for="list in item.cms" v-bind:key="list">- {{list}}</li>
+              <li v-for="list in item.cms" v-bind:key="list">- {{ list }}</li>
             </ul>
           </div>
         </div>
       </div>
-      <hr v-if="item.descripcion">
-      {{item.descripcion || ""}}
+      <hr v-if="item.descripcion" />
+      {{ item.descripcion || "" }}
     </ItemTimeline>
   </div>
 </template>
 
 <style scoped>
-  li {
-    margin-left: 15px;
-  }
+li {
+  margin-left: 15px;
+}
 
-
-  @media only screen and (max-width: 1024px) {
-    .icon {
-      color: #fff !important;
-    }
+@media only screen and (max-width: 1024px) {
+  .icon {
+    color: #fff !important;
   }
+}
 </style>
