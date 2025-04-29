91e6e2b931c83f844e66de0218c4e1ebb77f639a
Johan Román <r.johan95@gmail.com>
2025-04-29 18:54:31 -0400

    style: cambiar configuracion para que acepte nueva ruta de repositorio de git

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   // configuración de ruta base del proyecto
   // base: './', //configuracion para dar ruta base en caso de git page /<repositorio>/
-  base: '/portfolio/', //configuracion para dar ruta base en caso de git page /<repositorio>/
-  build: { 
+  base: "/portfolio-1_legacy/", //configuracion para dar ruta base en caso de git page /<repositorio>/
+  build: {
     // definición de la carpeta de compilado
     // configuracion para ejecutar en git pages
-    outDir: 'docs'
+    outDir: "docs",
   },
-})
+});
